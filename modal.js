a23e4f75b11671dcae9660c9eb144e33ff32a4bb
ismail <mukhammad.ismail@tripatra.com>
2026-06-17 06:35:44 +0700

    trisa

---
+++ modal.js
@@ -0,0 +1,14 @@
+const ACCESS_PASSWORD = "u-pride";
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const input = document.getElementById("accessCode").value;
+  if (input === ACCESS_PASSWORD) {
+    isLocked = false; 
+    document.getElementById("authModal").style.display = "none"; // unlock
+    animate(); // start animation
+    const audio = document.getElementById("bgAudio");
+    audio.play();
+  } else {
+    alert("Kamu siapa? hust.. pergi...");
+  }
+});
